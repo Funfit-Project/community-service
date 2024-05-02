def727e75cf247be8adf3ac4565ab071f67ab4ed
olsohee <dlthgml0108@daum.net>
2024-05-02 17:47:42 +0900

    feat: 사용자 서비스 분리

--- src/main/java/funfit/community/rabbitMq/dto/RequestUserByEmail.java
+++ src/main/java/funfit/community/rabbitMq/dto/RequestUserByEmail.java
@@ -1,15 +1,14 @@
-package funfit.community.user.dto;
+package funfit.community.rabbitMq.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-public class JoinResponse {
+public class RequestUserByEmail {
 
     private String email;
-    private String name;
-    private String role;
+    private String responseQueue; // 응답받을 큐 이름
 }
--- src/main/java/funfit/community/utils/JwtUtils.java
+++ src/main/java/funfit/community/utils/JwtUtils.java
@@ -2,8 +2,8 @@ package funfit.community.utils;
 
 import funfit.community.exception.ErrorCode;
 import funfit.community.exception.customException.CustomJwtException;
-import funfit.community.user.dto.JwtDto;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +12,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.time.Instant;
-import java.util.Date;
 
 @Slf4j
 @Component
@@ -25,42 +23,6 @@ public class JwtUtils {
     public JwtUtils(@Value("${jwt.secret}") String secretKey) {
         this.signingKey = Keys.hmacShaKeyFor(secretKey.getBytes());
         this.jwtParser = Jwts.parserBuilder().setSigningKey(signingKey).build();
-    }
-    
-    public JwtDto generateJwt(String email) {
-        Claims claims = Jwts.claims()
-                .setSubject(email);
-        return new JwtDto(generateAccessToken(claims), generateRefreshToken(claims));
-    }
-
-    private String generateAccessToken(Claims claims) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(Date.from(Instant.now().plusSeconds(60 * 60))) // 만료: 1시간
-                .signWith(SignatureAlgorithm.HS256, signingKey) // 사용할 암호화 알고리즘과 secret 값
-                .compact();
-    }
-
-    private String generateRefreshToken(Claims claims) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(Date.from(Instant.now()))
-                .setExpiration(Date.from(Instant.now().plusSeconds(60 * 60 * 24))) // 만료: 24시간
-                .signWith(SignatureAlgorithm.HS256, signingKey)
-                .compact();
-    }
-
-    public void validateAccessToken(HttpServletRequest request) {
-        log.info("access token 검증");
-        String accessToken = getJwtFromHeader(request);
-        try {
-            jwtParser.parseClaimsJws(accessToken);
-        } catch (ExpiredJwtException e) {
-            throw new CustomJwtException(ErrorCode.EXPIRED_JWT);
-        } catch (JwtException e) {
-            throw new CustomJwtException(ErrorCode.INVALID_JWT);
-        }
     }
 
     public String getEmailFromHeader(HttpServletRequest request) {
