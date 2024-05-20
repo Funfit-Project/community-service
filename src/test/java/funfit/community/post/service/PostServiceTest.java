c93392ad8fb5da721a1c32d73743f16058d13bbd
olsohee <dlthgml0108@daum.net>
2024-05-20 18:07:34 +0900

    test: 게시글 좋아요 기능 동시성 환경의 테스트 코드 작성

--- src/test/java/funfit/community/post/service/PostServiceTest.java
+++ src/test/java/funfit/community/post/service/PostServiceTest.java
@@ -4,15 +4,22 @@ import funfit.community.post.dto.CreatePostRequest;
 import funfit.community.post.entity.Category;
 import funfit.community.post.entity.Post;
 import funfit.community.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Transactional
 @SpringBootTest
@@ -20,6 +27,18 @@ class PostServiceTest {
 
     @Autowired private PostService postService;
     @Autowired private PostRepository postRepository;
+    @Autowired private InitService initService;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Autowired PostRepository postRepository;
+
+        @Bean
+        InitService initService() {
+            return new InitService(postRepository);
+        }
+    }
 
     @Test
     @DisplayName("게시글 등록 성공")
@@ -87,5 +106,40 @@ class PostServiceTest {
         // then
         Post savedPost = postRepository.findById(post.getId()).get();
         Assertions.assertThat(savedPost.getBookmarks().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("좋아요 등록 성공-한 사람이 동시에 여러번 좋아요를 눌러도 중복 저장되지 않아야 한다.")
+    void likePostSuccess() throws InterruptedException {
+        // post 엔티티 저장을 위한 메소드 호출
+        long savedPostId = initService.initPost();
+
+        int numberOfThreads = 10;
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.execute(() -> {
+                postService.likePost(savedPostId, "likeUser@naver.com");
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+
+        Post savedPost = postRepository.findById(savedPostId).get();
+        Assertions.assertThat(savedPost.getLikeCount()).isEqualTo(1);
+    }
+
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final PostRepository postRepository;
+
+        @Transactional(propagation = Propagation.REQUIRES_NEW)
+        public long initPost() {
+            Post post = Post.create("user@naver.com", "title", "content", Category.FREE);
+            postRepository.save(post);
+            return post.getId();
+        }
     }
 }
